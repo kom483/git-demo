2bdb3bde7c507ec83d6ea98bc196cbee5964e222
kom483 <preetkoaml1130@gmail.com>
2024-02-22 18:53:03 +0530

    add js program

---
+++ script.js
@@ -0,0 +1,16 @@
+//strings in js
+
+// let str = 'apnaCollege';
+// let str2 = "komalpreet";
+// console.log(str2 [7], str[5], str2[4]);
+
+//terplate literals in js
+let specialString = `This is a template string`;
+console.log(typeof specialString);
+
+let obj = {
+    item : "pen",
+    price : 10,
+};
+let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
