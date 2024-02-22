453ebc256b276733cd5660d7aa58eabe209a9ef3
kom483 <preetkoaml1130@gmail.com>
2024-02-22 19:17:21 +0530

    add template

--- script.js
+++ script.js
@@ -4,13 +4,8 @@
 // let str2 = "komalpreet";
 // console.log(str2 [7], str[5], str2[4]);
 
-//terplate literals in js
-let specialString = `This is a template string`;
-console.log(typeof specialString);
-
-let obj = {
-    item : "pen",
-    price : 10,
-};
-let output = `the cost of ${obj.item} is ${obj.price} rupees`;
-console.log(output);
+//template literals
+console.log(`this is template literals ${1+34+3+4}`);
+ 
+let str = "apna\tCollege";
+console.log(str);
